fe97d887de72e40d71606e04fdb6aaecb26ea431
Arthur Abdo <arthurabdo29@gmail.com>
2023-07-26 19:17:07 -0300

    Valida_questao inicio

--- EP2.py
+++ EP2.py
@@ -8,4 +8,19 @@ def transforma_base(lista):
         saida[nivel].append(questao)
     return saida
 
-    
+def valida_questao(dic):
+    saida = {}
+    titulo  = dic['titulo']
+    nivel = dic['nivel']
+    dic_opcoes = dic['opcoes']
+    correta = ['correta']
+    if titulo == '':
+        saida['titulo'] = 'nao_encontrado'
+    if titulo == ' ':
+        saida['titulo'] = 'vazio'
+    if nivel != 'facil' or nivel != 'medio ' or nivel != 'dificil':
+        saida['nivel'] = 'valor_errado'
+    if len(dic_opcoes) != 4:
+        saida['opcoes'] = 'tamanho_invalido'
+         
+         
