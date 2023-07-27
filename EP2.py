8564a6d552e0dadf53d260c3fb6e78d57231b5a7
Arthur Abdo <arthurabdo29@gmail.com>
2023-07-27 14:52:36 -0300

    passar questoes arquivo.py para main

--- EP2.py
+++ EP2.py
@@ -114,3 +114,527 @@ pulos = 3
 n_questao = 0
 acertos = 0
 end = False
+
+
+
+questoes = { {
+
+  "facil": [
+  {
+      "titulo": "Qual é o resultado de 3 + 7?",
+      "nivel": "facil",
+      "opcoes": {
+          "A": "9",
+          "B": "10",
+          "C": "11",
+          "D": "12"
+      },
+      "correta": "B"
+  },
+  {
+      "titulo": "Em que ano ocorreu a Revolução Francesa?",
+      "nivel": "facil",
+      "opcoes": {
+          "A": "1787",
+          "B": "1788",
+          "C": "1789",
+          "D": "1790"
+      },
+      "correta": "C"
+  },
+  {
+      "titulo": "Qual é o maior rio do mundo?",
+      "nivel": "facil",
+      "opcoes": {
+          "A": "Rio Nilo",
+          "B": "Rio Amazonas",
+          "C": "Rio Yangtzé",
+          "D": "Rio Mississipi"
+      },
+      "correta": "B"
+  },
+  {
+      "titulo": "Qual é o dobro de 15?",
+      "nivel": "facil",
+      "opcoes": {
+          "A": "20",
+          "B": "25",
+          "C": "30",
+          "D": "35"
+      },
+      "correta": "C"
+  },
+  {
+      "titulo": "Quem foi o primeiro presidente dos Estados Unidos?",
+      "nivel": "facil",
+      "opcoes": {
+          "A": "Thomas Jefferson",
+          "B": "George Washington",
+          "C": "Benjamin Franklin",
+          "D": "John Adams"
+      },
+      "correta": "B"
+  },
+  
+
+],
+"medio": [
+  {
+    "titulo": "Qual destes números é primo?",
+    "nivel": "medio",
+    "opcoes": {
+      "A": "259",
+      "B": "85",
+      "C": "49",
+      "D": "19"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "Na Conjectura de _______, escolhendo-se um número natural inicial n, onde n > 0, os seguintes critérios serão obedecidos: Se n for par o seu sucessor será a metade e se n for ímpar o seu sucessor será o triplo mais um, gerando então um novo número. Qual o nome da conjectura?",
+    "nivel": "medio",
+    "opcoes": {
+      "A": "Collatz",
+      "B": "Goldbach",
+      "C": "Poincaré",
+      "D": "Hodge"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual a segunda pessoa mais seguida no Instagram?",
+    "nivel": "medio",
+    "opcoes": {
+      "A": "Cristiano Ronaldo",
+      "B": "Dwayne Johnson",
+      "C": "Kim Kardashian",
+      "D": "Kylie Jenner"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "O que é um período de 1000 anos chamado?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Século",
+        "B": "Milênio",
+        "C": "Década",
+        "D": "Milenar"
+    },
+    "correta": "B"
+  },
+  {
+    "titulo": "Qual é o maior órgão do corpo humano?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Coração",
+        "B": "Fígado",
+        "C": "Pulmões",
+        "D": "Pele"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "Qual é o planeta mais próximo do Sol?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Marte",
+        "B": "Vênus",
+        "C": "Terra",
+        "D": "Mercúrio"
+    },
+    "correta": "D"
+  },
+  {
+  "titulo": "Qual é o maior animal terrestre?",
+  "nivel": "medio",
+  "opcoes": {
+      "A": "Elefante africano",
+      "B": "Girafa",
+      "C": "Baleia azul",
+      "D": "Rinoceronte branco"
+  },
+  "correta": "A"
+  },
+  {
+    "titulo": "Quem pintou a obra 'Mona Lisa'?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Vincent van Gogh",
+        "B": "Pablo Picasso",
+        "C": "Leonardo da Vinci",
+        "D": "Michelangelo"
+    },
+    "correta": "C"
+  },
+  {
+    "titulo": "Qual é o nome da famosa estátua da Grécia Antiga que foi esculpida em mármore de Paros?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Vênus de Milo",
+        "B": "Estátua da Liberdade",
+        "C": "David",
+        "D": "Cristo Redentor"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o maior país do mundo em área territorial?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Canadá",
+        "B": "Rússia",
+        "C": "Estados Unidos",
+        "D": "China"
+    },
+    "correta": "B"
+  },
+  {
+    "titulo": "Qual é a capital da Austrália?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Sydney",
+        "B": "Melbourne",
+        "C": "Canberra",
+        "D": "Brisbane"
+    },
+    "correta": "C"
+  },
+  {
+    "titulo": "Qual é o maior rio do mundo em volume de água?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Amazonas",
+        "B": "Nilo",
+        "C": "Mississipi",
+        "D": "Yangtzé"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o maior oceano da Terra?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Atlântico",
+        "B": "Índico",
+        "C": "Ártico",
+        "D": "Pacífico"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "Qual é a cidade mais populosa do mundo?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Tóquio",
+        "B": "Mumbai",
+        "C": "Nova Delhi",
+        "D": "São Paulo"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Quem foi o primeiro presidente do Brasil?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Getúlio Vargas",
+        "B": "Juscelino Kubitschek",
+        "C": "Prudente de Morais",
+        "D": "D. Pedro II"
+    },
+    "correta": "C"
+  },
+  {
+    "titulo": "Qual é a montanha mais alta do mundo?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Monte Everest",
+        "B": "Monte Kilimanjaro",
+        "C": "Monte Aconcágua",
+        "D": "Monte Denali"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o maior deserto do mundo?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Deserto do Saara",
+        "B": "Deserto do Atacama",
+        "C": "Deserto de Gobi",
+        "D": "Deserto da Arábia"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o elemento químico mais abundante na crosta terrestre?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Oxigênio",
+        "B": "Carbono",
+        "C": "Silício",
+        "D": "Alumínio"
+    },
+    "correta": "C"
+  },
+  {
+    "titulo": "Qual é o nome da famosa pintura que retrata a criação de Adão, feita por Michelangelo?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Última Ceia",
+        "B": "A Criação de Eva",
+        "C": "O Nascimento de Vênus",
+        "D": "A Criação de Adão"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "Qual é a língua mais falada no mundo?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Inglês",
+        "B": "Espanhol",
+        "C": "Hindi",
+        "D": "Mandarim"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "Qual é o maior animal do mundo?",
+    "nivel": "medio",
+    "opcoes": {
+        "A": "Elefante africano",
+        "B": "Tubarão-baleia",
+        "C": "Baleia azul",
+        "D": "Girafa"
+    },
+    "correta": "C"
+  }
+],
+"dificil": [
+  {
+    "titulo": "A reprodução dos seres vivos é um processo biológico através do qual os organismos geram descendência. Qual desta não é uma forma de reprodução assexuada?",
+    "nivel": "dificil",
+    "opcoes": {
+      "A": "Autogamia",
+      "B": "Esporulação",
+      "C": "Partenogênese",
+      "D": "Divisão binária"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual o resultado da operação 5 + 2 * 3 ^ 2, onde ^ representa potenciação?",
+    "nivel": "dificil",
+    "opcoes": {
+      "A": "441",
+      "B": "86",
+      "C": "Nenhuma das outras respostas",
+      "D": "23"
+    },
+    "correta": "D"
+  },
+  {
+    "titulo": "Qual é a maior cordilheira do mundo?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Cordilheira dos Andes",
+        "B": "Cordilheira do Himalaia",
+        "C": "Montanhas Rochosas",
+        "D": "Cordilheira dos Alpes"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é a maior bacia hidrográfica do mundo?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Bacia Amazônica",
+        "B": "Bacia do Congo",
+        "C": "Bacia do Rio da Prata",
+        "D": "Bacia do Nilo"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é a maior bacia hidrográfica do mundo?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Bacia Amazônica",
+        "B": "Bacia do Congo",
+        "C": "Bacia do Rio da Prata",
+        "D": "Bacia do Nilo"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o país com maior número de ilhas?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Filipinas",
+        "B": "Indonésia",
+        "C": "Japão",
+        "D": "Noruega"
+    },
+    "correta": "B"
+  },
+  {
+    "titulo": "Qual é a maior usina hidrelétrica do mundo?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Usina de Itaipu",
+        "B": "Usina de Três Gargantas",
+        "C": "Usina de Belo Monte",
+        "D": "Usina de Grand Coulee"
+    },
+    "correta": "B"
+  },
+  {
+    "titulo": "Quem foi o primeiro homem a caminhar na Lua?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Yuri Gagarin",
+        "B": "Buzz Aldrin",
+        "C": "Neil Armstrong",
+        "D": "Alan Shepard"
+    },
+    "correta": "C"
+  },
+  {
+    "titulo": "Qual é o país com maior número de vulcões ativos?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Indonésia",
+        "B": "Japão",
+        "C": "Itália",
+        "D": "Estados Unidos"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o único mamífero capaz de voar?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Morcego",
+        "B": "Esquilo",
+        "C": "Pterossauro",
+        "D": "Ave do paraíso"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "Qual é o maior desfiladeiro do mundo?",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Grand Canyon",
+        "B": "Cânion do Colca",
+        "C": "Desfiladeiro de Taroko",
+        "D": "Cânion de Blyde River"
+    },
+    "correta": "A"
+  },
+  {
+    "titulo": "complete: Chega de mentiras, de negar o meu desejo, eu te quuero mais que tudo, ____________",
+    "nivel": "dificil",
+    "opcoes": {
+        "A": "Eu preciso do seu beijo",
+        "B": "E euquero seu chamego",
+        "C": "Amanhã eu não te deixo",
+        "D": "Fico triste sem seu cheiro"
+    },
+    "correta": "A"
+
+    },
+    {
+      "titulo": "Qual é o livro mais vendido no mundo, após a Bíblia?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Cem Anos de Solidão",
+          "B": "Dom Quixote",
+          "C": "O Pequeno Príncipe",
+          "D": "O Alquimista"
+      },
+      "correta": "B"
+    },
+    {
+      "titulo": "Qual é a cidade mais antiga do mundo?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Jerusalém",
+          "B": "Atenas",
+          "C": "Roma",
+          "D": "Damasco"
+      },
+      "correta": "D"
+    },
+    {
+      "titulo": "Quem é o autor da obra '1984'?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Aldous Huxley",
+          "B": "George Orwell",
+          "C": "Ray Bradbury",
+          "D": "Kurt Vonnegut"
+      },
+      "correta": "B"
+    },
+    {
+      "titulo": "Qual é a montanha mais alta da América do Norte?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Monte McKinley (Denali)",
+          "B": "Monte Logan",
+          "C": "Monte Aconcágua",
+          "D": "Monte Whitney"
+      },
+      "correta": "A"
+    },
+    {
+      "titulo": "Quem foi o cientista que formulou a teoria da relatividade?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Isaac Newton",
+          "B": "Albert Einstein",
+          "C": "Niels Bohr",
+          "D": "Galileu Galilei"
+      },
+      "correta": "B"
+    },
+    {
+      "titulo": "Qual é a capital da Índia?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Mumbai",
+          "B": "Nova Delhi",
+          "C": "Bangalore",
+          "D": "Calcutá"
+      },
+      "correta": "B"
+    },
+    {
+      "titulo": "Qual é a maior ilha do mundo?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Groenlândia",
+          "B": "Bornéu",
+          "C": "Nova Guiné",
+          "D": "Madagascar"
+      },
+      "correta": "A"
+    },
+    {
+      "titulo": "Qual é o elemento químico mais leve?",
+      "nivel": "dificil",
+      "opcoes": {
+          "A": "Hidrogênio",
+          "B": "Hélio",
+          "C": "Lítio",
+          "D": "Oxigênio"
+      },
+      "correta": "A"
+    }
+]
+}
+
+}
+ 
